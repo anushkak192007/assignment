1b5675d5cd0381b44693d90d87b11ee527d5568a
anushkakumari <anushkad19208@gmail.com>
2026-01-29 13:59:20 +0530

    upgrade

---
+++ portfolio  &  html/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is some content in a React component.</p>
+    </div>
+  );
+}
+
+export default App;
